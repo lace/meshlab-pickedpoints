115512387a05d8d0f173ea69917bffc4486460b5
Paul Melnikow <email@paulmelnikow.com>
2021-08-28 12:26:46 -0400

    Reformat

--- meshlab_pickedpoints/cli.py
+++ meshlab_pickedpoints/cli.py
@@ -12,16 +12,16 @@ def convert_landmarks(pp_file, out):
     loaded = load(pp_file)
 
     result = [
-        {"name": name, "point": coords.tolist()}
-        for name, coords in loaded.items()
+        {"name": name, "point": coords.tolist()} for name, coords in loaded.items()
     ]
 
     if out is None:
         filename, _ = os.path.splitext(os.path.basename(pp_file))
         out = filename + ".json"
-    
+
     with open(out, "w") as f:
         json.dump(result, f)
+
 
 if __name__ == "__main__":  # pragma: no cover
     convert_landmarks()
